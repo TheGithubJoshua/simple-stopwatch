2c4488fba1ff20d291357e88d30456ff6d0b5202
TheGithubJoshua <joshuawhe@proton.me>
2023-05-26 18:54:08 +0100

    bare bones app

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet,Text,View, TouchableHighlight } from 'react-native';
+import { Stopwatch, Timer } from 'react-native-stopwatch-timer';
+ 
+class TestApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timerStart: false,
+      stopwatchStart: false,
+      totalDuration: 90000,
+      timerReset: false,
+      stopwatchReset: false,
+    };
+    this.toggleTimer = this.toggleTimer.bind(this);
+    this.resetTimer = this.resetTimer.bind(this);
+    this.toggleStopwatch = this.toggleStopwatch.bind(this);
+    this.resetStopwatch = this.resetStopwatch.bind(this);
+  }
+ 
+  toggleTimer() {
+    this.setState({timerStart: !this.state.timerStart, timerReset: false});
+  }
+ 
+  resetTimer() {
+    this.setState({timerStart: false, timerReset: true});
+  }
+ 
+  toggleStopwatch() {
+    this.setState({stopwatchStart: !this.state.stopwatchStart, stopwatchReset: false});
+  }
+ 
+  resetStopwatch() {
+    this.setState({stopwatchStart: false, stopwatchReset: true});
+  }
+  
+  getFormattedTime(time) {
+      this.currentTime = time;
+  };
+ 
+  render() {
+    return (
+      <View>
+        <Stopwatch laps msecs start={this.state.stopwatchStart}
+          reset={this.state.stopwatchReset}
+          options={options}
+          getTime={this.getFormattedTime} />
+        <TouchableHighlight onPress={this.toggleStopwatch}>
+          <Text style={{fontSize: 30}}>{!this.state.stopwatchStart ? "Start" : "Stop"}</Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={this.resetStopwatch}>
+          <Text style={{fontSize: 30}}>Reset</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
+ 
+const handleTimerComplete = () => alert("custom completion function");
+ 
+const options = {
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#000',
+    padding: 5,
+    borderRadius: 5,
+    width: 220,
   },
-});
+  text: {
+    fontSize: 30,
+    color: '#FFF',
+    marginLeft: 7,
+  }
+};
+ 
+AppRegistry.registerComponent('TestApp', () => TestApp);
+ 
