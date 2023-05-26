6a175df7ff353dafed668d89524df01a331adad2
TheGithubJoshua <joshuawhe@proton.me>
2023-05-26 19:49:10 +0100

    dd

--- App.js
+++ App.js
@@ -1,76 +1,109 @@
-import React, { Component } from 'react';
-import { AppRegistry, StyleSheet,Text,View, TouchableHighlight } from 'react-native';
-import { Stopwatch, Timer } from 'react-native-stopwatch-timer';
- 
-class TestApp extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      timerStart: false,
-      stopwatchStart: false,
-      totalDuration: 90000,
-      timerReset: false,
-      stopwatchReset: false,
-    };
-    this.toggleTimer = this.toggleTimer.bind(this);
-    this.resetTimer = this.resetTimer.bind(this);
-    this.toggleStopwatch = this.toggleStopwatch.bind(this);
-    this.resetStopwatch = this.resetStopwatch.bind(this);
-  }
- 
-  toggleTimer() {
-    this.setState({timerStart: !this.state.timerStart, timerReset: false});
-  }
- 
-  resetTimer() {
-    this.setState({timerStart: false, timerReset: true});
-  }
- 
-  toggleStopwatch() {
-    this.setState({stopwatchStart: !this.state.stopwatchStart, stopwatchReset: false});
-  }
- 
-  resetStopwatch() {
-    this.setState({stopwatchStart: false, stopwatchReset: true});
-  }
-  
-  getFormattedTime(time) {
-      this.currentTime = time;
-  };
- 
-  render() {
-    return (
-      <View>
-        <Stopwatch laps msecs start={this.state.stopwatchStart}
-          reset={this.state.stopwatchReset}
-          options={options}
-          getTime={this.getFormattedTime} />
-        <TouchableHighlight onPress={this.toggleStopwatch}>
-          <Text style={{fontSize: 30}}>{!this.state.stopwatchStart ? "Start" : "Stop"}</Text>
-        </TouchableHighlight>
-        <TouchableHighlight onPress={this.resetStopwatch}>
-          <Text style={{fontSize: 30}}>Reset</Text>
-        </TouchableHighlight>
+// Example of React Native Timer and Stopwatch
+// https://aboutreact.com/react-native-timer-stopwatch/
+
+// import React in our code
+import React, {useState} from 'react';
+
+// import all the components we are going to use
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+} from 'react-native';
+
+// importing library to use Stopwatch and Timer
+import {Stopwatch, Timer} from 'react-native-stopwatch-timer';
+
+const App = () => {
+  const [isTimerStart, setIsTimerStart] = useState(false);
+  const [isStopwatchStart, setIsStopwatchStart] = useState(false);
+  const [timerDuration, setTimerDuration] = useState(90000);
+  const [resetTimer, setResetTimer] = useState(false);
+  const [resetStopwatch, setResetStopwatch] = useState(false);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <Text style={styles.title}>
+          Example of React Native Timer and Stopwatch
+        </Text>
+        <View style={styles.sectionStyle}>
+          <Stopwatch
+            laps
+            msecs
+            start={isStopwatchStart}
+            // To start
+            reset={resetStopwatch}
+            // To reset
+            options={options}
+            // Options for the styling
+            getTime={(time) => {
+              console.log(time);
+            }}
+          />
+          <TouchableHighlight
+            onPress={() => {
+              setIsStopwatchStart(!isStopwatchStart);
+              setResetStopwatch(false);
+            }}>
+            <Text style={styles.buttonText}>
+              {!isStopwatchStart ? 'START' : 'STOP'}
+            </Text>
+          </TouchableHighlight>
+          <TouchableHighlight
+            onPress={() => {
+              setIsStopwatchStart(false);
+              setResetStopwatch(true);
+            }}>
+            <Text style={styles.buttonText}>RESET</Text>
+          </TouchableHighlight>
+        </View>
+        
       </View>
-    );
-  }
-}
- 
-const handleTimerComplete = () => alert("custom completion function");
- 
+    </SafeAreaView>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+    padding: 20,
+  },
+  sectionStyle: {
+    flex: 1,
+    marginTop: 32,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    fontSize: 20,
+    marginTop: 10,
+  },
+});
+
 const options = {
   container: {
-    backgroundColor: '#000',
+    backgroundColor: '#FF0000',
     padding: 5,
     borderRadius: 5,
-    width: 220,
+    width: 200,
+    alignItems: 'center',
   },
   text: {
-    fontSize: 30,
+    fontSize: 25,
     color: '#FFF',
     marginLeft: 7,
-  }
+  },
 };
- 
-AppRegistry.registerComponent('TestApp', () => TestApp);
- 
